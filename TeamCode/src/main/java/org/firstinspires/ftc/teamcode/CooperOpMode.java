3661ea3119c59951ab1c94c4a5b32f02657e11ee
Raja Khan <getrk@yahoo.com>
2017-11-06 20:34:51 -0500

    This is the test program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CooperOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CooperOpMode.java
@@ -3,31 +3,32 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
-@TeleOp(name = "TestDrive")
-public class TestDrive extends OpMode {
+/**
+ * Created by Raja on 11/6/2017.
+ */
+@TeleOp(name = "CooperOpmode")
+public class CooperOpMode extends OpMode{
 
-    final static double MOTOR_POWER = 0.15;
-
+    final static double MOTOR_POWER=0.15 ;
 
     DcMotor motorLeft;
     DcMotor motorRight;
-    //Servo arm;
+    DcMotor motorJack;
 
-
-    public TestDrive(){
-
-    }
+    public CooperOpMode(){}
 
     @Override
     public void init() {
         motorRight = hardwareMap.dcMotor.get("rightMotor");
         motorLeft = hardwareMap.dcMotor.get("leftMotor");
+        motorJack = hardwareMap.dcMotor.get("jackMotor");
         //arm = hardwareMap.servo.get("centerArm");
 
         motorLeft.setDirection(DcMotor.Direction.REVERSE);
         motorRight.setDirection(DcMotor.Direction.FORWARD);
+        motorJack.setDirection(DcMotor.Direction.REVERSE);
     }
 
     @Override
@@ -36,11 +37,13 @@ public class TestDrive extends OpMode {
         float leftY = gamepad1.left_stick_y;
         float rightY = gamepad1.right_stick_y;
         float leftX = gamepad1.left_stick_x;
+        float lefty = gamepad2.left_stick_y;
 
 
 
         motorLeft.setPower(leftY);
         motorRight.setPower(leftY);
+        motorJack.setPower(lefty);
         //motorRight.setPower(rightY);
         //arm.setPosition(rightY);
 
@@ -48,3 +51,4 @@ public class TestDrive extends OpMode {
 
 
 }
+
